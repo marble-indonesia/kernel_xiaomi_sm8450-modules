6f479aaa2a69ed2a1f7f26d9d424508612640456
pavelc4 <dimzzy23@gmail.com>
2025-07-13 00:22:33 +0700

    Move aw882xx headers to qcom directory and refactor macros
    Relocated aw882xx_log.h and aw882xx_spin.h from techpack/audio to qcom/opensource/audio-kernel for better organization. Additionally, simplified macro definitions by removing unnecessary `do-while` blocks, improving clarity and maintainability.

--- qcom/opensource/audio-kernel/asoc/codecs/aw882xx/inc/aw882xx_spin.h
+++ qcom/opensource/audio-kernel/asoc/codecs/aw882xx/inc/aw882xx_spin.h
@@ -1,9 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * aw882xx_spin.h spin_module
+ *
+ * Copyright (c) 2019 AWINIC Technology CO., LTD
+ *
+ * Author: Nick Li <liweilei@awinic.com.cn>
+ *
+ * This program is free software; you can redistribute  it and/or modify it
+ * under  the terms of  the GNU General  Public License as published by the
+ * Free Software Foundation;  either version 2 of the  License, or (at your
+ * option) any later version.
+ */
+
 #ifndef __AW882XX_SPIN_H__
 #define __AW882XX_SPIN_H__
 
 
 enum {
-	AW_SPIN_OFF_MODE= 0,
+	AW_SPIN_OFF_MODE = 0,
 	AW_ADSP_SPIN_MODE,
 	AW_REG_SPIN_MODE,
 	AW_REG_MIXER_SPIN_MODE,
