e5d4d7d9ae843d4b6893e93ed3014fa91c8a4c82
Maulana Riaddy <maulriaddy@gmail.com>
2026-03-11 05:17:02 +0100

    fixup! missing
    Signed-off-by: Maulana Riaddy <maulriaddy@gmail.com>

--- qcom/opensource/display-drivers/msm/dsi/dsi_ctrl_hw_cmn.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_ctrl_hw_cmn.c
@@ -525,13 +525,6 @@ void dsi_ctrl_hw_cmn_set_video_timing(struct dsi_ctrl_hw *ctrl,
 
 	dsi_ctrl = container_of(ctrl, struct dsi_ctrl, hw);
 	/* TODO: HS TIMER value? */
-	if (dsi_ctrl->max_hs_timer_supported)
-		/*
-		* Setting HS TIMER value assuming maximum
-		* HS transmission time required will be 40ms
-		*/
-		DSI_W32(ctrl, DSI_HS_TIMER_CTRL, 0x4BB80);
-	else
 		DSI_W32(ctrl, DSI_HS_TIMER_CTRL, 0x3FD08);
 
 	DSI_W32(ctrl, DSI_MISR_VIDEO_CTRL, 0x10100);
@@ -655,11 +648,6 @@ void dsi_ctrl_hw_cmn_setup_cmd_stream(struct dsi_ctrl_hw *ctrl,
 	}
 
 	dsi_ctrl = container_of(ctrl, struct dsi_ctrl, hw);
-	/* HS Timer value */
-	if (dsi_ctrl->max_hs_timer_supported)
-		DSI_W32(ctrl, DSI_HS_TIMER_CTRL, 0x4BB80);
-	else
-		DSI_W32(ctrl, DSI_HS_TIMER_CTRL, 0x3FD08);
 
 	stream_ctrl = (stride_final + 1) << 16;
 	stream_ctrl |= (vc_id & 0x3) << 8;
